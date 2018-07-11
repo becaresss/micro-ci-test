ccf8ada060636073a39edcd8b0fcb18476de5cb2
ahernandez-stratio <ahernandez@stratio.com>
2018-07-11 17:45:15 +0200

    Added microservices parent

--- src/main/java/com/stratio/aip/MicroCiTestApplication.java
+++ src/main/java/com/stratio/aip/MicroCiTestApplication.java
@@ -1,13 +1,11 @@
 package com.stratio.aip;
 
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnablePrometheusEndpoint
 public class MicroCiTestApplication {
 
     public static void main(String[] args) {
